58cc8911ded5741a85446dfe7aabf6f51821d61c
Christian Finel <macreiben@gmail.com>
2024-11-18 08:51:41 +0100

    added cors support

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import helloController from "./controllers/hellocontroller";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
 app.get("/hello", helloController);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
